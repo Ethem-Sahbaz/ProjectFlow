115457832e79ac14e8bf86b1fc0898d89f435c46
Ethem Sahbaz <ethemsahbaz@outlook.com>
2025-01-31 05:04:49 +0100

    Implemented get projects endpoint.

--- src/ProjectFlow.Api/Program.cs
+++ src/ProjectFlow.Api/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using ProjectFlow.Application;
+using ProjectFlow.Application.Services.Projects.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
@@ -16,29 +20,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/projects", async (IProjectsReader reader) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var projects = await reader.GetAllProjectsAsync();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Ok(projects);
 })
-.WithName("GetWeatherForecast")
+.WithName("GetAllProjects")
 .WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
